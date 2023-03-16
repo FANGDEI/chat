1e85ced2c874a7feb04751cce30298f63d3d3acc
FANGDEI <1639579565@qq.com>
2023-03-16 19:43:50 +0800

    # 添加事务处理

--- internal/pkg/localer/friend.go
+++ internal/pkg/localer/friend.go
@@ -4,33 +4,42 @@ type Friend struct {
 	ID       int64 `json:"id"`
 	UserID   int64 `json:"user_id"`
 	FriendID int64 `json:"friend_id"`
-	Agree    bool  `json:"agree"`
 }
 
 var friend = "friend"
 
 func (m *Manager) CreateFriend(userID, friendID int64) error {
-	return m.handler.Table(friend).Create(&Friend{
-		UserID:   userID,
-		FriendID: friendID,
-	}).Error
+	err := m.execTx(func(m *Manager) error {
+		err := m.handler.Table(friend).Create(&Friend{
+			UserID:   userID,
+			FriendID: friendID,
+		}).Error
+		if err != nil {
+			return err
+		}
+		err = m.handler.Table(friend).Create(&Friend{
+			UserID:   friendID,
+			FriendID: userID,
+		}).Error
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	return err
 }
 
 func (m *Manager) DeleteFriend(userID, friendID int64) error {
-	return m.handler.Table(friend).Where("user_id = ? AND friend_id = ?", userID, friendID).Delete(&Friend{}).Error
-}
-
-// TODO:
-func (m *Manager) AcceptFriend(selfID, otherID int64) error {
-	return m.handler.Table(friend).Where("user_id = ? AND friend_id = ?", selfID, otherID).Update("agree", 1).Error
-}
-
-func (m *Manager) DeAcceptFriend(selfID, otherID int64) error {
-	return m.handler.Table(friend).Where("user_id = ? AND friend_id = ?", selfID, otherID).Delete(&Friend{}).Error
-}
-
-func (m *Manager) FriendExists(userID, friendID int64) bool {
-	var u SimpleUser
-	err := m.handler.Table(friend).Where("user_id = ? AND friend_id = ? AND agree = 1", userID, friendID).Take(&u).Error
-	return (err == nil) && (u.ID != 0)
+	err := m.execTx(func(m *Manager) error {
+		err := m.handler.Table(friend).Where("user_id = ? AND friend_id = ?", userID, friendID).Delete(&Friend{}).Error
+		if err != nil {
+			return err
+		}
+		err = m.handler.Table(friend).Where("user_id = ? AND friend_id = ?", friendID, userID).Delete(&Friend{}).Error
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	return err
 }
